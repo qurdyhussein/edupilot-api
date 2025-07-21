4fdb1d832d229c520c0bf64169345481adf27ea4
Hussein Qurdy <qurdyhussein@gmail.com>
2025-07-21 15:21:25 +0300

    Imported StudentLoginSerializer for login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import StudentRegistrationSerializer
+from .serializers import StudentRegistrationSerializer, StudentLoginSerializer
 
 class StudentRegistrationView(APIView):
     def post(self, request):

5f1357463db6971e8f9fa720b7d7e64e778d1c33
Hussein Qurdy <qurdyhussein@gmail.com>
2025-07-21 11:57:01 +0300

    Fixed incorrect include of accounts.urls

--- edupilot_api/urls.py
+++ edupilot_api/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),  # ← ← ← Ongeza hii hapa
+    path('api/', include('students.urls')),  # ← ← ← Ongeza hii hapa
 
 ]

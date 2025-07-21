abf17610e0e488964c168bd405b50d74d68ea397
Hussein Qurdy <qurdyhussein@gmail.com>
2025-07-21 09:18:48 +0300

    Configured DEBUG using env variable

--- edupilot_api/settings.py
+++ edupilot_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os  # ← hakikisha line hii ipo juu ya file
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e6_2=*^sa-&en-==)@-$v6yzn5hq0z1_$9sd3_2l$@3)j(6-pd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition

ca034e370b927d697550dd25d2cc28a11f715423
Hussein Qurdy <qurdyhussein@gmail.com>
2025-07-21 11:07:38 +0300

    Added student login route

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from .views import StudentLoginView
 
 urlpatterns = [
     path('register/', StudentRegistrationView.as_view(), name='student-register'),
-    path('login/', StudentLoginView.as_view(), name='student-login'),
+    path('api/login/', StudentLoginView.as_view(), name='student-login'),
 
 ]
